b15c639de00331e14ebc42d71ba6618a2dc09069
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-12 00:20:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,12 @@ attr_accessor :email_addresses
   end
 
   def parse
-    @email_addresses.split(", ")
+    array = []
+    comma = @email_addresses.split(", ")
+    white_space = @email_addresses.split(" ")
+    array << comma
+    array << white_space
+    array
   end
 
 
